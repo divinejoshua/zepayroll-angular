191b9846335a36ac04b85ddd5de16bf03c9f9e12
divinejoshua <divineuche399@gmail.com>
2023-06-03 12:24:25 +0100

    Added payout and home page

---
+++ src/app/pages/payout/payout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payout',
+  templateUrl: './payout.component.html',
+  styleUrls: ['./payout.component.css']
+})
+export class PayoutComponent {
+
+}
