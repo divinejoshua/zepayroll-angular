005b9018269b7a89ad6da4a3f09cdcb84cd9b398
divinejoshua <divineuche399@gmail.com>
2023-06-03 20:35:39 +0100

    made changes

--- src/app/pages/accounts/login/login.component.ts
+++ src/app/pages/accounts/login/login.component.ts
@@ -8,12 +8,12 @@ import { FormGroup, FormControl, Validators} from '@angular/forms';
 })
 export class LoginComponent {
 
-  showPassword: boolean = false;
+    showPassword: boolean = false;
 
     // Form group
     loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email] ),
-      password:  new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z ]*')]),
+      password:  new FormControl('', [Validators.required, Validators.nullValidator, Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$/)]),
     });
 
     constructor(){}
@@ -22,6 +22,9 @@ export class LoginComponent {
     // onSubmit form
     onSubmit() :void {
       console.log(this.loginForm.value)
+      console.log(this.loginForm.controls.email.errors?.['required'])
     }
 
 }
+
+
