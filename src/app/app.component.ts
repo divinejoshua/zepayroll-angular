75caa3d13071aaf178944e36c6ec12f1feac5962
divinejoshua <divineuche399@gmail.com>
2023-06-03 13:33:54 +0100

    added splash screen loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'zepayroll-angular';
+  initialLoading : boolean = true;
+
+  onAppLoad (){
+     // Terminate the loader
+    //  setTimeout(() => this.initialLoading = false, 2000);
+  }
+
+  ngOnInit() {
+    this.onAppLoad()
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        default : '#14B8A6',
+        secondary : '#37474F',
+      },
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '1200px',
+        xl: '1440px',
+      },
+    },
   },
   plugins: [],
 }
