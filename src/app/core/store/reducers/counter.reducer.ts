3cfc013025682fade35f86b564e4b2833ea8496a
divinejoshua <divineuche399@gmail.com>
2023-07-14 18:52:50 +0100

    Added normal functions instead of arrow functions

--- src/app/core/store/reducers/counter.reducer.ts
+++ src/app/core/store/reducers/counter.reducer.ts
@@ -4,9 +4,21 @@ import { increment, decrement, reset } from '../actions/counter.action';
 export const initialState = 3846;
 
 const _counterReducer = createReducer(initialState,
-  on(increment, state => state + 1),
-  on(decrement, state => state - 1),
-  on(reset, state => 0),
+
+  // Increment function
+  on(increment, function(state) {
+    return state + 1;
+  }),
+
+  //Decrement function
+  on(decrement, function(state) {
+    return state - 1;
+  }),
+
+  // Reset counter
+  on(reset, function() {
+    return 0;
+  }),
 );
 
 export function counterReducer(state : any, action: any) {
